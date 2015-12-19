1689405c83ca2de4f8ab7678c5155030ec20421a
william1024 <tngwilliam1024@gmail.com>
2015-12-19 11:25:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ var ctx = canvas.getContext("2d");
 
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
-var heroImg = document.createElement("img");
-heroImg.src = "images/jason.gif";
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower-bin.png";
 
-var hero = {
+var tower = {
   x: 0,
   y: 0
 };
 
 function draw(){
   ctx.drawImage(bgImg,0,0);
-  ctx.drawImage(heroImg, hero.x, hero.y);
+  ctx.drawImage(towerImg, hero.x, hero.y);
 }
 
 // setTimeout(draw,1000);
